f25f4b92eff98eef68ea012bd1e504fb19e2b568
Kelvin Nguyen <knguyen@student.neumont.edu>
2015-04-07 12:34:25 -0600

    Add current capstone project to repository

---
+++ Capstone1/Capstone1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Capstone1.Model;
+
+namespace Capstone1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string str = "https://www.google.com/search?q=";
+        //string str = "https://duckduckgo.com/?q=";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WorkerClass wc = new WorkerClass();
+
+            //RichText.Text = wc.getSourceCode("www.google.com");
+            //.RichText.RichText.t = wc.getSourceCode("www.google.com");
+            //RichText.Document.Blocks.Clear();
+            //RichText.Document.Blocks.Add(new Paragraph(new Run(wc.getSourceCode("http://www.bongda.com.vn"))));
+
+            if(EnterBox.Text != "")
+            {
+                Dictionary<string, string> contentList = new Dictionary<string, string>();
+               // RichText.Document.Blocks.Clear();
+                contentList["address"] = EnterBox.Text;
+                RichText.Document.Blocks.Clear();
+                string content = wc.getContentOnly(str + EnterBox.Text);
+                //string content = wc.getAllConnectingUrl2(str + EnterBox.Text);
+
+                Paragraph p = new Paragraph(new Run(content));
+                RichText.Document.Blocks.Add(p);
+
+            }
+        }
+    }
+}
---
+++ Capstone1/Capstone1/Model/WorkerClass.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+namespace Capstone1.Model
+{
+    public class WorkerClass
+    {
+        public string getSourceCode(string url)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            string str = sr.ReadToEnd();
+            sr.Close();
+
+
+
+            return str;
+        }
+
+        public string getContentOnly(string url)
+        {
+            string reg = @"(<span.*?>.*?</span>)";
+            string source = getSourceCode(url);
+            MatchCollection m1 = Regex.Matches(source, reg, RegexOptions.Singleline);
+            string result = "";
+            foreach(Match group in m1)
+            {
+                string data = Regex.Replace(group.Value, @"<(.|\n)*?>", string.Empty);
+                result += data+" \n";
+            }
+            return result;
+        }
+
+        public string getAllConnectingUrl(string url)
+        {
+            string regex = @"(<a.*?>.*?<b>.*?</b>.*?</a>)";
+            string regexURL = @"herf= ";
+            string source = getSourceCode(url);
+            MatchCollection m1 = Regex.Matches(source, regex, RegexOptions.Singleline);
+            string result = "";
+            foreach (Match group in m1)
+            {
+               // string data = Regex.Replace(group.Value, @"<(.|\n)*?>", string.Empty);
+                //result += data + " \n";
+                result += group.ToString()+"\n";
+            }
+            return result;
+           // return "";
+        }
+
+        //using htmlAgilityPack
+        public string getAllConnectingUrl2(string url)
+        {
+            //get DOM elements of a url
+            HtmlWeb htmlDoc = new HtmlWeb();
+            var document = htmlDoc.Load(url);
+            var aTags = document.DocumentNode.SelectNodes("//a");
+            int count = 1;
+            string result ="";
+            if (aTags != null)
+            {
+                foreach (var tag in aTags)
+                {
+                    result += count + "." + tag.InnerHtml+"\n";
+
+                    result += count + ". " + tag.InnerHtml + " - " + tag.Attributes["href"].Value + "\n";
+                     //HtmlAttribute col = tag.Attributes["href"] ;
+                     //result += col.Value;
+                    
+                    count++;
+                }
+                return result;
+            }
+            else
+            {
+                return "nothing";
+            }
+           
+        }
+    }
+}
